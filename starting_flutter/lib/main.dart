7332282fdbdde34600e49f71857148d53ddc87ac
neresleonardo <leonardo.borges.neres@gmail.com>
2022-11-01 09:01:43 -0300

    starting flutter

---
+++ starting_flutter/lib/main.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/material.dart';
+
+main( ) {
+  runApp(new PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Olá Flutter!!!')
+    );
+  }
+}
