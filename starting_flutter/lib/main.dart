cdc93464ff2d3c28dfb0f97d92086484185e400a
neresleonardo <leonardo.borges.neres@gmail.com>
2022-11-02 15:24:31 -0300

    user for

--- starting_flutter/lib/main.dart
+++ starting_flutter/lib/main.dart
@@ -47,6 +47,12 @@ class _PerguntaAppState extends State<PerguntaApp> {
       }
     ];
 
+    List<Widget> respostas = [];
+
+    for (var textoResp in perguntas[_perguntaSelecionanda]['resposta']) {
+      respostas.add(Resposta(textoResp, _responder));
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -55,9 +61,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: <Widget>[
             Questao(perguntas[_perguntaSelecionanda]['texto']),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
+            ...respostas,
           ],
         ),
       ),
